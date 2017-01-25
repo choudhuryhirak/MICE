73b3519c4b89b3744b60918c7583c221fa2cea65
choudhuryhirak <choudhuryhirakkumar@gmail.com>
2017-01-25 14:29:43 +0530

    formval

--- validation.php
+++ validation.php
@@ -1,24 +1,28 @@
 <?php
     
-    $value = $_GET['query'];
     $formfield = $_GET['field'];
-    // Check Valid or Invalid user name when user enters user name in username input field.
-    // Check Valid or Invalid password when user enters password in password input field.
-    if ($formfield == "pass_aj") {
-    if (strlen($value) < 6) {
-    echo "Password too short";
-    } else {
-    echo "Strong";
-    }
-    }
-    // Check Valid or Invalid email when user enters email in email input field.
-    if ($formfield == "email_aj") {
-    if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $value)) {
-    echo "Invalid email";
-    } else {
-    echo "Valid";
-    }
-    }
-    
+    $value = $_GET['query'];
+
+        if ($formfield == "pass_aj") {
+        if (strlen($value) < 6) {
+        echo "Password too short";
+        } else {
+        echo "Strong";
+            
+        }
+        }
+
+        if ($formfield == "pass_aj2") {
+        if (strlen($value) < 6) {
+        echo "Password too short";
+        }
+            else 
+        {
+            echo "Strong";
+            
+        }
+        }
+
+        
     
 ?>
